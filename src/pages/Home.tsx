f54419f5c2b2741e9006b5d85087dc7505154c1b
Drishyaat <110720115+Drishyaat@users.noreply.github.com>
2025-01-02 07:10:21 +0000

    changed url to local img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import mainBg from "../assets/mainBg.jpg";
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-[url('https://wallpapers.com/images/featured/dark-and-blue-aesthetic-laptop-15hvaune8yxlqlss.jpg')] text-white min-h-screen bg-cover bg-center">
+    <div
+      className="text-white min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${mainBg})`,
+      }}
+    >
       <Navbar />
       <div className="flex items-center justify-center min-h-screen">
         <div className="bg-black bg-opacity-60 rounded-lg p-12 sm:p-16 lg:p-20 max-w-2xl w-full mx-4 text-center">
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,81 +1,10 @@
-// import React from "react";
-// import Navbar from "../components/Navbar";
-// import bookshelf from "../assets/bookshelf.png";
-
-// const projects = [
-//   {
-//     id: 1,
-//     title: "Book Shelf",
-//     description:
-//       "An e-commerce website designed to enhance user experience with features like a secure login and registration system, intuitive shopping cart, wishlist for saving favorite items, and a reliable payment gateway.",
-//     link: "https://bookkshelf.netlify.app/",
-//   },
-//   {
-//     id: 2,
-//     title: "IMDB Clone",
-//     description: `A clone of IMDb featuring a user-friendly interface that allows users to easily browse movies and shows. Key functionalities include "Add to Favorites" and "Add to Watchlist," enabling users to curate their viewing preferences seamlessly. The project showcases skills in front-end development and user experience design.`,
-//     link: "https://yy6q43.csb.app/",
-//   },
-//   {
-//     id: 3,
-//     title: "Mind sage",
-//     description:
-//       "A meditation website that offers a comprehensive collection of links to various meditation techniques, promoting mindfulness and relaxation. The site features a built-in timer, allowing users to customize their meditation sessions effortlessly. This project highlights my focus on user experience and well-being.",
-//     link: "https://qv8jw4.csb.app/",
-//   },
-//   {
-//     id: 4,
-//     title: "Facebook Login page clone",
-//     description:
-//       "A front-end project that replicates the Facebook login page, built using Tailwind CSS for a responsive and modern design. This project demonstrates my skills in creating intuitive user interfaces and applying utility-first styling to enhance visual appeal.",
-//     link: "https://gwlw6q-5173.csb.app/",
-//   },
-//   // {
-//   //   id: 5,
-//   //   title: "Project Five",
-//   //   description: "Description for project five.",
-//   //   link: "#",
-//   // },
-//   // {
-//   //   id: 6,
-//   //   title: "Project Six",
-//   //   description: "Description for project six.",
-//   //   link: "#",
-//   // },
-// ];
-
-// const Project: React.FC = () => {
-//   return (
-//     <div className="bg-[url('https://www.pixelstalk.net/wp-content/uploads/2016/03/Free-Dark-Blue-Wallpaper-High-Quality-download.png')] bg-cover bg-center text-white min-h-screen">
-//       <Navbar />
-//       <div className="container mx-auto p-6 md:p-8 lg:p-10 bg-black bg-opacity-50 rounded-lg">
-//         <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
-//         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-//           {projects.map((project) => (
-//             <div
-//               key={project.id}
-//               className="bg-gray-800 rounded-lg p-5 transition-transform transform hover:scale-105 opacity-90 hover:opacity-100"
-//             >
-//               <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-//               <p className="mb-4">{project.description}</p>
-//               <a href={project.link} className="text-blue-400 hover:underline">
-//                 View Project
-//               </a>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Project;
 import React from "react";
 import Navbar from "../components/Navbar";
 import bookshelf from "../assets/bookshelf.png";
 import imdb from "../assets/imdb.png";
-import meditationImg from "../assets/meditationImg.png";
+import wellnestImg from "../assets/wellnest.png";
 import fb from "../assets/fb.png";
+import projectBg from "../assets/projectBg.png";
 
 const projects = [
   {
@@ -88,18 +17,18 @@ const projects = [
   },
   {
     id: 2,
-    title: "IMDB Clone",
-    description: `A clone of IMDb featuring a user-friendly interface that allows users to easily browse movies and shows. Key functionalities include "Add to Favorites" and "Add to Watchlist," enabling users to curate their viewing preferences seamlessly. The project showcases skills in front-end development and user experience design.`,
-    link: "https://yy6q43.csb.app/",
-    image: imdb,
+    title: "Wellnest",
+    description:
+      "Developed a user-friendly fitness and wellness platform with dedicated pages for meditation, nutrition, and workout routines. Key features include user profile management, a workout journal to track progress, a BMI calculator for health insights, and secure login/sign-up functionality for personalized access. ",
+    link: "https://wellnest04.netlify.app/",
+    image: wellnestImg,
   },
   {
     id: 3,
-    title: "Mind sage",
-    description:
-      "A meditation website that offers a comprehensive collection of links to various meditation techniques, promoting mindfulness and relaxation. The site features a built-in timer, allowing users to customize their meditation sessions effortlessly. This project highlights my focus on user experience and well-being.",
-    link: "https://qv8jw4.csb.app/",
-    image: meditationImg,
+    title: "IMDB Clone",
+    description: `A clone of IMDb featuring a user-friendly interface that allows users to easily browse movies and shows. Key functionalities include "Add to Favorites" and "Add to Watchlist," enabling users to curate their viewing preferences seamlessly. The project showcases skills in front-end development and user experience design.`,
+    link: "https://imdbclone24.netlify.app/",
+    image: imdb,
   },
   {
     id: 4,
@@ -113,7 +42,12 @@ const projects = [
 
 const Project: React.FC = () => {
   return (
-    <div className="bg-[url('https://www.pixelstalk.net/wp-content/uploads/2016/03/Free-Dark-Blue-Wallpaper-High-Quality-download.png')] bg-cover bg-center text-white min-h-screen">
+    <div
+      className="bg-cover bg-center text-white min-h-screen"
+      style={{
+        backgroundImage: `url(${projectBg})`,
+      }}
+    >
       <Navbar />
       <div className="container mx-auto p-6 md:p-8 lg:p-10 bg-black bg-opacity-50 rounded-lg">
         <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
